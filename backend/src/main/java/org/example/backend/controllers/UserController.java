3ce07daa60778baee8a467da0f7e5c47f044e691
Ramon Trekovanicz <ramesz6@me.com>
2024-10-01 18:18:27 +0200

    setup usercontroller

--- backend/src/main/java/org/example/backend/controllers/UserController.java
+++ backend/src/main/java/org/example/backend/controllers/UserController.java
@@ -1,4 +1,12 @@
 package org.example.backend.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.backend.services.UserService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
 }
