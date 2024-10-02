d07ca94fb145e3c59103fe2b6c11d34018f8c821
Ramon Trekovanicz <ramesz6@me.com>
2024-10-02 13:59:38 +0200

    add logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css';
 import { useState } from 'react';
+import gpbLogo from './assets/gbp-logo.jpg';
+
 
 function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (e : React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
   return (
+
     <div className="container">
+      <div>
+        <img className='gbp-logo' src={gpbLogo} alt="GBP Logo" />
+      </div>
       <h1>Gr33nBay Project</h1>
       <form onSubmit={handleLogin} className="login-form">
         <div className="input-group">
